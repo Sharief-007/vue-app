2eafc644a5703de27698f53d37fdb427d75ead20
sharief <shariefmohammad007@gmail.com>
2020-10-31 01:34:29 +0530

    mm

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <div class="sidebar bg-dark">
+      <div class="header">
+<!--        <b-avatar size="lg" class="prof"></b-avatar>-->
+        <h3 class="prof" style="color: white">Chat Party</h3>
+      </div>
+      <Nav />
+    </div>
+    <div class="main">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import Nav from './components/Nav'
+export default {
+  name: "App",
+  components: {
+    Nav
+  }
+}
+</script>
+<style scoped>
+*, body{
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+}
+
+.sidebar{
+  flex: 0.15;
+  /*display: flex;*/
+  /*border-right: 1px solid silver;*/
+}
+
+.header {
+  display: flex;
+  height: 100px;
+}
+
+.prof {
+  margin: auto;
+}
+
+.main {
+  flex: 1;
+  background: white;
+}
+
+@media only screen and (max-width: 900px) {
+  .sidebar {
+    flex: 0;
+  }
+}
+</style>
---
+++ src/components/Recepient.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="root d-flex align-items-center">
+  <div class="image ">
+    <b-avatar size="50px" variant="primary"></b-avatar>
+  </div>
+  <div class="d-flex flex-column pl-1 flex-grow-1">
+    <h2 class="p-0 m-0">Sharief</h2>
+    <p>Message mess lorem ipsum</p>
+  </div>
+  <div class="d-flex flex-grow-1">
+    <b-badge variant="primary" style="margin: auto">5</b-badge>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Recepient"
+}
+</script>
+
+<style scoped>
+.root {
+  padding: 0 5px;
+  /*border-bottom: 2px solid #edece8;*/
+  background-color: #292b2c;
+  color: #feefef;
+}
+
+.root:hover {
+  /*background: #edece8;*/
+  background-color: rgba(0,0,0,0.75);
+  cursor: pointer;
+}
+</style>
